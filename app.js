19cdf1f29766b1db569afd23eaccc34a70ab1795
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-11-05 00:33:03 +0200

    add base type

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+// let age = 50;
+var age;
+// let name = "Max";
+var nameUser;
+// let toggle = true;
+var toggle;
+// let empty = null;
+var empty;
+// let notInitialize = undefined;
+var notInitialize;
+var callback;
+//  let callback = (a) => {
+//  return 100 + a;
+//  };
+// let anything = -20;
+// anything = "Text";
+// anything = {};
+var anything;
+var some;
+some = "Text";
+var str;
+if (typeof some === "string")
+    str = some;
+// let person = ["Max", 21];
+var person;
--- app.ts
+++ app.ts
@@ -9,3 +9,32 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
+
+// let age = 50;
+let age: number;
+// let name = "Max";
+let nameUser: string;
+// let toggle = true;
+let toggle: boolean;
+// let empty = null;
+let empty: null;
+// let notInitialize = undefined;
+let notInitialize: undefined;
+
+let callback: (a: number) => number;
+//  let callback = (a) => {
+//  return 100 + a;
+//  };
+
+// let anything = -20;
+// anything = "Text";
+// anything = {};
+let anything: any;
+
+let some: unknown;
+some = "Text";
+let str: string;
+if (typeof some === "string") str = some;
+
+// let person = ["Max", 21];
+let person: [string, number];
