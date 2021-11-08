83de3a3834096bf39aeb18c4536a76ce5beb16cc
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-11-08 23:02:09 +0200

    add button for debug test

---
+++ dist/button.js
@@ -0,0 +1,6 @@
+"use strict";
+const buttonTest = document.querySelector("#buttonFormId");
+buttonTest === null || buttonTest === void 0 ? void 0 : buttonTest.addEventListener("click", () => {
+    console.log("Click");
+});
+//# sourceMappingURL=button.js.map
---
+++ src/button.ts
@@ -0,0 +1,5 @@
+const buttonTest = document.querySelector("#buttonFormId");
+
+buttonTest?.addEventListener("click", () => {
+  console.log("Click");
+});
