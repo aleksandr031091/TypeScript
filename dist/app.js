49c9e318432fe276efd70ba850481e5c3ec99f28
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-11-08 21:10:07 +0200

    add tsconfig

---
+++ dist/app.js
@@ -0,0 +1,57 @@
+"use strict";
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
+});
+let age;
+let nameUser;
+let toggle;
+let empty;
+let notInitialize;
+let callback;
+let anything;
+let some;
+some = "Text";
+let str;
+if (typeof some === "string")
+    str = some;
+let person;
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+const load = {
+    status: Status.LOADING,
+};
+if (load.status === Status.LOADING) {
+    console.log("Сраница загружается");
+}
+else {
+    console.log("Сраница загружена");
+}
+let init;
+init = "String";
+init = 3;
+let value;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
+const page2 = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "open",
+};
+//# sourceMappingURL=app.js.map
